0741e8056cac2aca345a51f99ca771e7c7f51709
jenhakk <jenna20100@student.hamk.fi>
2022-04-22 16:06:35 +0300

    testing

--- TheElectionMachineRest/src/main/java/dao/Dao.java
+++ TheElectionMachineRest/src/main/java/dao/Dao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import data.*;
-//testaus
+//testaussss
 public class Dao {
 	private String url;
 	private String user;
