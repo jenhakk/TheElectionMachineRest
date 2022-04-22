d7ed2db469f1b6765b2555362adc199e60c27b68
A-d-f <annamaria.nappila@gmail.com>
2022-04-22 15:24:31 +0300

    Update Dao.java

--- TheElectionMachineRest/src/main/java/dao/Dao.java
+++ TheElectionMachineRest/src/main/java/dao/Dao.java
@@ -47,7 +47,8 @@ public class Dao {
 	// ArrayList. First creates a new ArrayList, then connects to a database. After
 	// executing the
 	// given statement, creates a new object Candidate and then adds this object to
-	// an ArrayList named list
+	// an ArrayList named list.
+	// And more
 	// -Ansku
 	public ArrayList<Candidate> readAllCand() {
 		ArrayList<Candidate> list = new ArrayList<>();
